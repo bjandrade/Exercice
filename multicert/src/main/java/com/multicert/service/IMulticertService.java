267dcb3cc78b1301dc79a7146e421bbdc7b7e358
bjandrade <bruno.jesus88@gmail.com>
2017-07-01 12:34:41 +0100

    unused files removed

--- multicert/src/main/java/com/multicert/service/IMulticertService.java
+++
@@ -1,8 +0,0 @@
-package com.multicert.service;
-
-import com.multicert.bean.MulticertResponse;
-
-public interface IMulticertService {
-
-	public MulticertResponse getCountryInfo(String countryCode) throws Exception;
-}
